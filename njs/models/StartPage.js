2962386b7930896c76663f75027906dd3459d8da
Gleb Arestov <gr1b0k@gmail.com>
2013-09-18 20:54:24 +0400

    css

--- njs/models/StartPage.js
+++ njs/models/StartPage.js
@@ -134,7 +134,7 @@ BrowseMap.Model.extendTo(StartPage, {
 					});
 				} else {
 					result.items.unshift({
-						label: selectByNum(result.count, no_flabel),
+						label: 'Все' + selectByNum(result.count, no_flabel),
 						novalue: true,
 						counter: result.count
 					});
