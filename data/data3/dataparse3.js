04060138e86ff615f89992da8a2e98c7b911fe36
Gleb Arestov <gr1b0k@gmail.com>
2013-09-20 19:49:21 +0400

    datafixes

--- data/data3/dataparse3.js
+++ data/data3/dataparse3.js
@@ -1,5 +1,11 @@
 (function(){
 "use strict";
+for (var i = 0; i < window.jsondata.items.length; i++) {
+	var cur = window.jsondata.items[i];
+	cur.pos = cur.gender_pos;
+	//window.jsondata[i]
+}
+
 window.onmessage = function(e) {
 	
 	if (e.data == 'send_data'){
