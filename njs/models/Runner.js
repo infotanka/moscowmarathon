3dc7a789edf6c60014d0ef727f396d1872e3f8a4
Gleb Arestov <gr1b0k@gmail.com>
2013-09-23 12:01:04 +0400

    result time string

--- njs/models/Runner.js
+++ njs/models/Runner.js
@@ -1,4 +1,4 @@
-define(['../libs/BrowseMap'], function(BrowseMap) {
+define(['../libs/BrowseMap', 'moment'], function(BrowseMap, moment) {
 "use strict";
 
 var Runner = function() {};
@@ -9,6 +9,31 @@ BrowseMap.Model.extendTo(Runner, {
 		this.mapStates(this.raw_map_st, raw, true);
 		this.initStates();
 		this.wch(this.map_parent, 'selected_filter_gender');
+
+
+
+		/*		var tstr;
+				if (from_start) {
+					tstr = moment(cvs_data.start_time)
+					.startOf('day')
+					.add( cur.relative_to_start)
+					.format(cur.last ? 'HH:mm:ss' : 'HH:mm');
+				} else {
+					tstr = moment(cur.relative_to_day)
+					.format(cur.last ? 'HH:mm:ss' : 'HH:mm');
+				}*/
+	},
+	'compx-full_time_string':{
+		depends_on: ['start_time', 'result_time'],
+		fn: function(start_time, result_time) {
+			if (start_time && result_time){
+				var tstr = moment(start_time)
+					.startOf('day')
+					.add( result_time * 1000 )
+					.format('H:mm:ss');
+				return tstr;
+			}
+		}
 	},
 	raw_map_st: {
 		pos: 'pos',
@@ -22,7 +47,8 @@ BrowseMap.Model.extendTo(Runner, {
 		team: 'team',
 		result_time: 'result_time',
 		gender: 'gender',
-		full_time_string: 'result_time_string'
+		start_time: 'start_time',
+	//	full_time_string: 'result_time_string'
 	}
 
 	//Ctry✌✄✌Region✌✄✌City✌✄✌Club✌✄✌Result✌✄✄✌Group✌✄✌P.on g.✌✄✌Netto time✌✄✄
